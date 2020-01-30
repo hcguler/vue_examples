ccb0ca27665e3400690ab64ef95cbac0f8e7bc63
hcguler <cihad_guler@hotmail.com>
2020-01-30 23:28:02 +0300

    component

---
+++ src/basic-examples/10-component/example1/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <app-servers></app-servers>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/basic-examples/10-component/example1/src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-server-status></app-server-status>
+        <app-server-status></app-server-status>
+        <app-server-status></app-server-status>
+        <app-server-status></app-server-status>
+    </div>
+</template>
+
+<script>
+
+    import ServerStatus from "./components/ServerStatus.vue";
+
+    export default {
+        components: {
+            'app-server-status': ServerStatus
+        }
+    }
+</script>
+
+<style>
+</style>
